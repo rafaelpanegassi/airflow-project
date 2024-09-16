813a7baa82e8407cc580d781b9c8e4e1d8cf35a1
Rafael Panegassi <rm.panegassi@gmail.com>
2024-09-15 23:26:22 -0300

    update

--- dags/capturar_pokemon.py
+++ dags/capturar_pokemon.py
@@ -1,19 +1,19 @@
-
 from airflow.decorators import task, dag
-
 from include.controller import gerar_numero_aleatorio, fetch_pokemon_data, add_pokemon_to_db
-
 from datetime import datetime
 
-@dag(dag_it='api_to_database',
-     description='pipeline_para_capturar_pokemon',
-     start_date=datetime(2024,9,15),
-     schedule='* * * * *',
-     catchup=False)
+@dag(dag_id="capturar_pokemon",
+     description="pipeline_para_capturar_pokemon",
+     start_date=datetime(2024, 9, 15),
+     schedule="* * * * *",
+     catchup=False
+)
+
 def api_to_database():
 
     @task(task_id='gerar_numero_aleatorio')
     def task_gerar_numero_aleatorio():
+
         return gerar_numero_aleatorio()
 
     @task(task_id='fetch_pokemon_data')
@@ -24,9 +24,9 @@ def api_to_database():
     def task_add_pokemon_to_db(pokemon_data):
         return add_pokemon_to_db(pokemon_data)
 
-    t1 = task_gerar_numero_aleatorio
-    t2 = task_fetch_pokemon_data
-    t3 = task_add_pokemon_to_db
+    t1 = task_gerar_numero_aleatorio()
+    t2 = task_fetch_pokemon_data(t1)
+    t3 = task_add_pokemon_to_db(t2)
 
     t1 >> t2 >> t3
 
